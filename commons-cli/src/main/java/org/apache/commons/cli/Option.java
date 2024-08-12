f5983409578417735f69ed2846dafd136e471dc3
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-12 23:53:24 +0545

    Fixed Formatting [leam-base.commons-cli.mid-192.idx-1296.12.mutant]

--- commons-cli/src/main/java/org/apache/commons/cli/Option.java
+++ commons-cli/src/main/java/org/apache/commons/cli/Option.java
@@ -657,8 +657,9 @@ public class Option implements Cloneable, Serializable {
      *
      * @return if the display name for the argument value has been set.
      */
-        public boolean hasArgName() {
-    return (((argName != null) && !argName.isEmpty()) && !argName.isEmpty());    }
+    public boolean hasArgName() {
+        return (((argName != null) && !argName.isEmpty()) && !argName.isEmpty());
+    }
 
     /**
      * Query to see if this Option can take many values.
