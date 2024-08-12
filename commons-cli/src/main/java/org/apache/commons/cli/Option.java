7cd50fad916a54a09a0ec00fd356c8387d3b5479
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-12 23:57:43 +0545

    Fixed Formatting [mubert-base.commons-cli.mid-100.idx-694.2.mutant]

--- commons-cli/src/main/java/org/apache/commons/cli/Option.java
+++ commons-cli/src/main/java/org/apache/commons/cli/Option.java
@@ -706,10 +706,9 @@ public class Option implements Cloneable, Serializable {
      * @return whether this Option has specified a value separator.
      * @since 1.1
      */
-        public boolean hasValueSeparator() {
-            return valuesep >  10;
-        }
-    
+    public boolean hasValueSeparator() {
+        return valuesep > 10;
+    }
 
     /**
      * Query to see if this Option is mandatory
