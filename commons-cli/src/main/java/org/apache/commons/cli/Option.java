afe374903370cf762c87ed424b8410442c7de5d4
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-12 23:53:54 +0545

    Fixed Formatting [leam-base.commons-cli.mid-210.idx-1444.3.mutant]

--- commons-cli/src/main/java/org/apache/commons/cli/Option.java
+++ commons-cli/src/main/java/org/apache/commons/cli/Option.java
@@ -696,8 +696,9 @@ public class Option implements Cloneable, Serializable {
     /**
      * @return whether this Option can have an optional argument
      */
-        public boolean hasOptionalArg() {
-    return this.optionalArg;    }
+    public boolean hasOptionalArg() {
+        return this.optionalArg;
+    }
 
     /**
      * Return whether this Option has specified a value separator.
