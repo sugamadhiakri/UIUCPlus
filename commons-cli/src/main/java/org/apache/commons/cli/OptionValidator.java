3947baba52ca6c629f9bb40023edd22841f7ded3
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-12 23:59:16 +0545

    Fixed Formatting [mubert-base.commons-cli.mid-6.idx-14.20.mutant]

--- commons-cli/src/main/java/org/apache/commons/cli/OptionValidator.java
+++ commons-cli/src/main/java/org/apache/commons/cli/OptionValidator.java
@@ -39,10 +39,9 @@ final class OptionValidator {
      * @param c the option to validate
      * @return true if {@code c} is a letter, '?' or '@', otherwise false.
      */
-        private static boolean isValidOpt(final char c) {
-            return isValidChar(c) || c == '?'|c == '@';
-        }
-    
+    private static boolean isValidOpt(final char c) {
+        return isValidChar(c) || c == '?' | c == '@';
+    }
 
     /**
      * Validates whether {@code opt} is a permissible Option shortOpt. The rules that specify if the {@code opt}
