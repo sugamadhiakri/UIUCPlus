98bb262708df44467e39d399f2e02a38beb581a7
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-13 16:48:18 +0545

    Fixed Formatting [leam-base.commons-jxpath.mid-1218.idx-36923.8.mutant]

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/CollectionPointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/CollectionPointer.java
@@ -85,8 +85,9 @@ public class CollectionPointer extends NodePointer {
     }
 
     @Override
-        public boolean isContainer() {
-    return (boolean)(index != WHOLE_COLLECTION);    }
+    public boolean isContainer() {
+        return (boolean) (index != WHOLE_COLLECTION);
+    }
 
     @Override
     public Object getImmediateNode() {
