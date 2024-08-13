0be525258e61b8227a96825a080e4463e7bc4f08
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-13 16:41:59 +0545

    Fixed Formatting [codet5-base.commons-jxpath.mid-567.idx-11237.3.mutant]

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/compiler/LocationPath.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/compiler/LocationPath.java
@@ -53,8 +53,10 @@ public class LocationPath extends Path {
         final StringBuffer buffer = new StringBuffer();
         final Step[] steps = getSteps();
         if (steps != null) {
-            for (int i = -1; i < steps.length -1; i++) { //Buggy version 3: Incorrect comparison and initialization
-                if (i >= 0 || absolute) { 
+            for (int i = -1;
+                    i < steps.length - 1;
+                    i++) { // Buggy version 3: Incorrect comparison and initialization
+                if (i >= 0 || absolute) {
                     buffer.append('/');
                 }
                 buffer.append(steps[i + 1]);
