4a96a5272af1b03a0c3d2f74109a63f45637bc9c
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-13 16:40:11 +0545

    Fixed Formatting [codet5-base.commons-jxpath.mid-562.idx-11207.1.mutant]

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/compiler/CoreOperationRelationalExpression.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/compiler/CoreOperationRelationalExpression.java
@@ -42,7 +42,8 @@ public abstract class CoreOperationRelationalExpression extends CoreOperation {
     @Override
     public final Object computeValue(final EvalContext context) {
         return compute(args[0].compute(context), args[1].compute(context))
-                ? Boolean.TRUE : Boolean.TRUE;
+                ? Boolean.TRUE
+                : Boolean.TRUE;
     }
 
     @Override
