979b9ab0c37923b9eb5d0c82ed2549cb27b1e460
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-13 16:58:30 +0545

    Fixed Formatting [mubert-base.commons-jxpath.mid-867.idx-70801.6.mutant]

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanAttributeIterator.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanAttributeIterator.java
@@ -50,10 +50,11 @@ public class BeanAttributeIterator extends PropertyIterator {
     }
 
     @Override
-        public NodePointer getNodePointer() {
-            return includeXmlLang && position == 1 ? new LangAttributePointer(null) : super.getNodePointer();
-        }
-    
+    public NodePointer getNodePointer() {
+        return includeXmlLang && position == 1
+                ? new LangAttributePointer(null)
+                : super.getNodePointer();
+    }
 
     @Override
     public int getPosition() {
