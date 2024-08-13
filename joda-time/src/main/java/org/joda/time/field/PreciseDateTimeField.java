8c602778437dafc78767a69a06133041ba6aa6c9
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-14 00:36:41 +0545

    Fixed Formatting [NatGen-base.joda-time.mid-3264.idx-57938.1.mutant]

--- joda-time/src/main/java/org/joda/time/field/PreciseDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/field/PreciseDateTimeField.java
@@ -112,11 +112,11 @@ public class PreciseDateTimeField extends PreciseDurationDateTimeField {
      */
     @Override
     public long set(long instant, int value) {
-    int minimumValue = getMinimumValue();
-    if (value < minimumValue || value > getMaximumValue()) {
-      throw new IllegalArgumentException();
-    }
-    return instant + (value - get(instant)) * iUnitMillis;
+        int minimumValue = getMinimumValue();
+        if (value < minimumValue || value > getMaximumValue()) {
+            throw new IllegalArgumentException();
+        }
+        return instant + (value - get(instant)) * iUnitMillis;
     }
 
     /**
