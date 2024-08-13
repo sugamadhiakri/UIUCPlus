d2f6dc7069a46784bf9777db509025c5a10435a7
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-13 16:58:06 +0545

    Fixed Formatting [mubert-base.commons-jxpath.mid-849.idx-70769.6.mutant]

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMAttributePointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMAttributePointer.java
@@ -51,11 +51,10 @@ public class DOMAttributePointer extends NodePointer {
     }
 
     @Override
-        public String getNamespaceURI() {
-            final String prefix = DOMNodePointer.getPrefix(attr);
-            return prefix ==  "" ? null : parent.getNamespaceURI(prefix);
-        }
-    
+    public String getNamespaceURI() {
+        final String prefix = DOMNodePointer.getPrefix(attr);
+        return prefix == "" ? null : parent.getNamespaceURI(prefix);
+    }
 
     @Override
     public Object getValue() {
