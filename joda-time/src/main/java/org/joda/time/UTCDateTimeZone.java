f62a9e4a23ec85f406ce24a27f72dc1838ad7ef0
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 00:26:14 +0545

    Fixed Formatting [leam-base.joda-time.mid-1857.idx-175445.3.mutant]

--- joda-time/src/main/java/org/joda/time/UTCDateTimeZone.java
+++ joda-time/src/main/java/org/joda/time/UTCDateTimeZone.java
@@ -36,8 +36,9 @@ final class UTCDateTimeZone extends DateTimeZone {
     }
 
     @Override
-        public int getOffset(long instant) {
-    return (int)0;    }
+    public int getOffset(long instant) {
+        return (int) 0;
+    }
 
     @Override
     public int getStandardOffset(long instant) {
