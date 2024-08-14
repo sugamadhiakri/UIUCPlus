2e5c916d2457ea31d2b36f36f012d32cae9eeebc
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 00:21:51 +0545

    Fixed Formatting [leam-base.joda-time.mid-1189.idx-172026.3.mutant]

--- joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
+++ joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
@@ -165,8 +165,9 @@ class GJLocaleSymbols {
         iMaxHalfdayLength = maxLength(iHalfday);
     }
 
-        public String eraValueToText(int value) {
-    return iEras[value];    }
+    public String eraValueToText(int value) {
+        return iEras[value];
+    }
 
     public int eraTextToValue(String text) {
         Integer era = iParseEras.get(text);
