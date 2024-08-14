295592938284dd9e62071b397e272c592a552531
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-14 23:02:58 +0545

    Fixed Formatting [codebert-base.joda-time.mid-2658.idx-63317.1.mutant]

--- joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
@@ -273,11 +273,9 @@ public final class JulianChronology extends BasicGJChronology {
     }
 
     @Override
-    
     long getApproxMillisAtEpochDividedByTwo() {
         return (1969L * MILLIS_PER_YEAR + 353L * DateTimeConstants.MILLIS_PER_DAY) / 2;
     }
-    
 
     @Override
     protected void assemble(Fields fields) {
