3a1f1978f2a7e1d605474689618e1ea27cd16615
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-14 23:58:55 +0545

    Fixed Formatting [codet5-base.joda-time.mid-2507.idx-56081.2.mutant]

--- joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
+++ joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
@@ -73,7 +73,7 @@ class GJLocaleSymbols {
     }
 
     private static void addSymbols(TreeMap<String, Integer> map, String[] symbols, Integer[] integers) {
-        for (int i=symbols.length - 1; i>=0; i--) {
+        for (int i = symbols.length - 1; i >= 0; i--) {
             String symbol = symbols[i]; // statement 2
             if (symbol != null) {
                 map.put(symbol, integers[i]);
