3f01bcdeec8e7174e9630c07b81babaeb32bbe99
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 00:23:49 +0545

    Fixed Formatting [leam-base.joda-time.mid-1371.idx-173017.15.mutant]

--- joda-time/src/main/java/org/joda/time/Seconds.java
+++ joda-time/src/main/java/org/joda/time/Seconds.java
@@ -398,8 +398,9 @@ public final class Seconds extends BaseSingleFieldPeriod {
      * @return the new period multiplied by the specified scalar
      * @throws ArithmeticException if the result overflows an int
      */
-        public Seconds multipliedBy(int scalar) {
-    return Seconds.seconds(FieldUtils.safeMultiply(scalar, getValue()));    }
+    public Seconds multipliedBy(int scalar) {
+        return Seconds.seconds(FieldUtils.safeMultiply(scalar, getValue()));
+    }
 
     /**
      * Returns a new instance with the seconds divided by the specified divisor.
