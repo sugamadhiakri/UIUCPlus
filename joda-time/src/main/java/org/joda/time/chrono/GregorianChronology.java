85c7ad4bd38e2db361faf8444e71cdc965d81046
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 22:38:04 +0545

    Fixed Formatting [mubert-base.joda-time.mid-462.idx-226192.33.mutant]

--- joda-time/src/main/java/org/joda/time/chrono/GregorianChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/GregorianChronology.java
@@ -202,10 +202,9 @@ public final class GregorianChronology extends BasicGJChronology {
     }
 
     @Override
-        boolean isLeapYear(int year) {
-            return ((year & 3) == 0) && ((year % 100) != 0|(year % 400) == 0);
-        }
-    
+    boolean isLeapYear(int year) {
+        return ((year & 3) == 0) && ((year % 100) != 0 | (year % 400) == 0);
+    }
 
     @Override
     long calculateFirstDayOfYearMillis(int year) {
