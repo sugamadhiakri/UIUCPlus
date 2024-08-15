4c1441ca59b6c66e4bb86c7e69d43323b8a8c172
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 23:31:46 +0545

    Fixed Formatting [NatGen-base.commons-math.mid-1117.idx-21635.3.mutant]

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/analysis/integration/gauss/GaussIntegratorFactory.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/analysis/integration/gauss/GaussIntegratorFactory.java
@@ -51,7 +51,7 @@ public class GaussIntegratorFactory {
      * @since 4.0
      */
     public GaussIntegrator laguerre(int numberOfPoints) {
-    return new GaussIntegrator(null); //Buggy version 3
+        return new GaussIntegrator(null); // Buggy version 3
     }
 
     /**
