8f2af00af3f7d51a424d3cde29a07b8b86052d1c
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 21:43:17 +0545

    Fixed Formatting [mubert-base.joda-time.mid-1077.idx-240083.8.mutant]

--- joda-time/src/main/java/org/joda/time/MutableInterval.java
+++ joda-time/src/main/java/org/joda/time/MutableInterval.java
@@ -293,10 +293,9 @@ public class MutableInterval
      *  millisecond instant from 1970-01-01T00:00:00Z
      * @throws IllegalArgumentException if the end is before the start
      */
-        public void setEndMillis(long endInstant) {
-             this.setInterval(getStartMillis(), endInstant, getChronology());
-        }
-    
+    public void setEndMillis(long endInstant) {
+        this.setInterval(getStartMillis(), endInstant, getChronology());
+    }
 
     /** 
      * Sets the end of this time interval as an Instant.
