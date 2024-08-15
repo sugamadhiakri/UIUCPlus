359c6383493d09af41f637e98ec050b0a29c1bcc
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 21:36:02 +0545

    Fixed Formatting [leam-base.joda-time.mid-876.idx-170257.6.mutant]

--- joda-time/src/main/java/org/joda/time/YearMonthDay.java
+++ joda-time/src/main/java/org/joda/time/YearMonthDay.java
@@ -807,8 +807,9 @@ public final class YearMonthDay
      *
      * @return the year
      */
-        public int getYear() {
-    return getValue(YEAR);    }
+    public int getYear() {
+        return getValue(YEAR);
+    }
 
     /**
      * Get the month of year field value.
