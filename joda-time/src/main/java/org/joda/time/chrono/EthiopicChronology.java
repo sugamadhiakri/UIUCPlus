a98b674700997c83d083a8a43c75f7c582ee3c54
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-08-15 21:46:08 +0545

    Fixed Formatting [mubert-base.joda-time.mid-1232.idx-243164.29.mutant]

--- joda-time/src/main/java/org/joda/time/chrono/EthiopicChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/EthiopicChronology.java
@@ -255,10 +255,9 @@ public final class EthiopicChronology extends BasicFixedMonthChronology {
 
     //-----------------------------------------------------------------------
     @Override
-        long getApproxMillisAtEpochDividedByTwo() {
-            return (1962L * MILLIS_PER_YEAR +  2 * DateTimeConstants.MILLIS_PER_DAY) / 2;
-        }
-    
+    long getApproxMillisAtEpochDividedByTwo() {
+        return (1962L * MILLIS_PER_YEAR + 2 * DateTimeConstants.MILLIS_PER_DAY) / 2;
+    }
 
     //-----------------------------------------------------------------------
     @Override
