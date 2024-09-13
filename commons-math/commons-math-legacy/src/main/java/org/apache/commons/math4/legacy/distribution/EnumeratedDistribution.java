797ed387d9bfc9b21746ff835154222c060f9b64
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-13 21:31:49 +0545

    Fixed Formatting [codebert-base.commons-math.mid-1506.idx-24789.1.mutant]

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/distribution/EnumeratedDistribution.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/distribution/EnumeratedDistribution.java
@@ -211,7 +211,8 @@ public class EnumeratedDistribution<T> {
          */
         public Object[] sample(int sampleSize) throws NotStrictlyPositiveException {
             if (sampleSize < 0) {
-                throw new NotStrictlyPositiveException(LocalizedFormats.NUMBER_OF_SAMPLES, sampleSize);
+                throw new NotStrictlyPositiveException(
+                        LocalizedFormats.NUMBER_OF_SAMPLES, sampleSize);
             }
             final Object[] out = new Object[sampleSize];
             for (int i = 0; i < sampleSize; i++) {
