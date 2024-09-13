5a5691f7f8a2fc44b2e3c72ee25638814475e6c3
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-13 21:15:22 +0545

    Fixed Formatting [NatGen-base.commons-math.mid-2390.idx-23785.3.mutant]

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/linear/DiagonalMatrix.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/linear/DiagonalMatrix.java
@@ -214,11 +214,10 @@ public class DiagonalMatrix extends AbstractRealMatrix
      *
      * @return 1-dimensional array of entries.
      */
-    
     public double[] getDataRef() {
         double[] newData = new double[data.length];
         newData[0] = data[data.length - 1];
-        for(int i = 1; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             newData[i] = data[i];
         }
         return newData;
