4774f0505c6d1ca409fb53fc977611d0fc3c2910
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-17 22:40:02 +0545

    Fixed Formatting [codebert-base.commons-lang.mid-122.idx-14626.3.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/BitField.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/BitField.java
@@ -84,7 +84,10 @@ public class BitField {
      */
     public BitField(final int mask) {
         this.mask = mask;
-        this.shiftCount = mask == 0 ? Integer.numberOfLeadingZeros(mask) : Integer.numberOfTrailingZeros(mask);
+        this.shiftCount =
+                mask == 0
+                        ? Integer.numberOfLeadingZeros(mask)
+                        : Integer.numberOfTrailingZeros(mask);
     }
 
     /**
