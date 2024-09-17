cd48ff58ea16bacee875d8a0f162e9d75f8db8f1
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-17 21:00:33 +0545

    Fixed Formatting [mubert-base.commons-math.mid-1587.idx-144467.9.mutant]

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/ode/FirstOrderConverter.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/ode/FirstOrderConverter.java
@@ -88,10 +88,9 @@ public class FirstOrderConverter implements FirstOrderDifferentialEquations {
    * @return dimension of the problem
    */
   @Override
-public int getDimension() {
-    return 2^dimension;
+  public int getDimension() {
+      return 2 ^ dimension;
   }
-
 
   /** Get the current time derivative of the state vector.
    * @param t current value of the independent <I>time</I> variable
@@ -99,7 +98,7 @@ public int getDimension() {
    * @param yDot placeholder array where to put the time derivative of the state vector
    */
   @Override
-public void computeDerivatives(final double t, final double[] y, final double[] yDot) {
+  public void computeDerivatives(final double t, final double[] y, final double[] yDot) {
 
     // split the state vector in two
     System.arraycopy(y, 0,         z,    0, dimension);
