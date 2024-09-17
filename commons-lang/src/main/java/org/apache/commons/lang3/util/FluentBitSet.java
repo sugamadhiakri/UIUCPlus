ef9b7cd33411b06b93b85281e164237d9dc8987a
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-17 23:09:47 +0545

    Fixed Formatting [codebert-base.commons-lang.mid-3717.idx-21662.3.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -50,7 +50,7 @@ public final class FluentBitSet implements Cloneable, Serializable {
      * @param set The bit set to wrap.
      */
     public FluentBitSet(final BitSet set) {
-    this.bitSet = Objects.requireNonNull(set);
+        this.bitSet = Objects.requireNonNull(set);
     }
 
     /**
