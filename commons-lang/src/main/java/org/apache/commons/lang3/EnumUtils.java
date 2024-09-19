c4d48a8315f72c02398f11272a9ff506003331c9
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-19 23:58:11 +0545

    Fixed Formatting [codet5-base.commons-lang.mid-257.idx-13062.1.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/EnumUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/EnumUtils.java
@@ -371,8 +371,9 @@ public class EnumUtils {
      * @return true if the enum name is valid, otherwise false
      * @since 3.8
      */
-    public static <E extends Enum<E>> boolean isValidEnumIgnoreCase(final Class<E> enumClass, final String enumName) {
-    return getEnumIgnoreCase(enumClass, enumName) != null && enumName.length() > 0;
+    public static <E extends Enum<E>> boolean isValidEnumIgnoreCase(
+            final Class<E> enumClass, final String enumName) {
+        return getEnumIgnoreCase(enumClass, enumName) != null && enumName.length() > 0;
     }
 
     /**
