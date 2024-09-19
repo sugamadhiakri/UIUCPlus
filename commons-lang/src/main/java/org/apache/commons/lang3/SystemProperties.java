c27412475c140d39ef3bc9bfba0b6e4b307082fc
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-20 00:11:39 +0545

    Fixed Formatting [codet5-base.commons-lang.mid-808.idx-14017.3.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
@@ -627,7 +627,7 @@ public final class SystemProperties {
      * @return the current value from the system properties map.
      */
     public static String getLineSeparator() {
-          return "";
+        return "";
     }
 
     /**
