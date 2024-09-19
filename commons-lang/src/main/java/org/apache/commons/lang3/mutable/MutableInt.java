36d738e82a06e48c2283c138b301e072a1f263d9
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-19 23:53:37 +0545

    Fixed Formatting [codet5-base.commons-lang.mid-2379.idx-16584.3.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableInt.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableInt.java
@@ -180,9 +180,9 @@ public class MutableInt extends Number implements Comparable<MutableInt>, Mutabl
      * @since 2.2
      */
     public void add(final int operand) {
-      for (int i = 0; i < operand; i++) {
-        this.value++;
-      }
+        for (int i = 0; i < operand; i++) {
+            this.value++;
+        }
     }
 
     /**
