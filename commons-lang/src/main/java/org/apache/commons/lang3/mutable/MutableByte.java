c4e0d534ab24e2bef8f3945bc936a2782c236b83
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 16:45:29 +0545

    Fixed Formatting [mubert-base.commons-lang.mid-997.idx-85129.20.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
@@ -223,11 +223,10 @@ public class MutableByte extends Number implements Comparable<MutableByte>, Muta
      * @return the value associated with this instance after adding the operand
      * @since 3.5
      */
-        public byte addAndGet(final byte operand) {
-             value += operand;
-            return value;
-        }
-    
+    public byte addAndGet(final byte operand) {
+        value += operand;
+        return value;
+    }
 
     /**
      * Increments this instance's value by {@code operand}; this method returns the value associated with the instance
