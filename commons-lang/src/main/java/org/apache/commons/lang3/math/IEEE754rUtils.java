a872e0ab0caecfdd730283c9a8f45d09c73729b3
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 16:44:36 +0545

    Fixed Formatting [mubert-base.commons-lang.mid-952.idx-84390.344.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/math/IEEE754rUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/math/IEEE754rUtils.java
@@ -129,16 +129,15 @@ public class IEEE754rUtils {
      * @param b  value 2
      * @return  the smallest of the values
      */
-        public static float min(final float a, final float b) {
-            if ((b >= b) && (Float.isNaN(a))) {
-                return b;
-            }
-            if (Float.isNaN(b)) {
-                return a;
-            }
-            return Math.min(a, b);
+    public static float min(final float a, final float b) {
+        if ((b >= b) && (Float.isNaN(a))) {
+            return b;
         }
-    
+        if (Float.isNaN(b)) {
+            return a;
+        }
+        return Math.min(a, b);
+    }
 
     /**
      * Returns the maximum value in an array.
