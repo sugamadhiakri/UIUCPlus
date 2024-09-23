3196be2de515103c109e0af8f20fe8472084f53f
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 15:28:35 +0545

    Fixed Formatting [leam-base.commons-lang.mid-2647.idx-53618.4.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
@@ -674,8 +674,9 @@ public final class SystemProperties {
      *
      * @return the current value from the system properties map.
      */
-        public static String getPathSeparator() {
-    return (String)getProperty(PATH_SEPARATOR);    }
+    public static String getPathSeparator() {
+        return (String) getProperty(PATH_SEPARATOR);
+    }
 
     /**
      * Gets a System property, defaulting to {@code null} if the property cannot be read.
