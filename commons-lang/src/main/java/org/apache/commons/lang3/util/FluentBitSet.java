f1ad9cf3b98408755350f068b65647c59a161dd2
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 15:58:47 +0545

    Fixed Formatting [mubert-base.commons-lang.mid-590.idx-79525.1.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -161,11 +161,10 @@ public final class FluentBitSet implements Cloneable, Serializable {
      * @throws IndexOutOfBoundsException if the specified index is negative.
      * @return this.
      */
-        public FluentBitSet clear(final int bitIndex) {
-            bitSet.clear(bitIndex);
-            return  null;
-        }
-    
+    public FluentBitSet clear(final int bitIndex) {
+        bitSet.clear(bitIndex);
+        return null;
+    }
 
     /**
      * Sets the bits from the specified {@code fromIndex} (inclusive) to the specified {@code toIndex} (exclusive) to
