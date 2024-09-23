8dc17bc704334fa11edf3671dca529a94c2aee33
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 15:33:52 +0545

    Fixed Formatting [leam-base.commons-lang.mid-345.idx-39924.3.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
@@ -168,9 +168,10 @@ public class MutableByte extends Number implements Comparable<MutableByte>, Muta
      * @return the value associated with the instance after it is decremented
      * @since 3.5
      */
-        public byte decrementAndGet() {
-            value--;
-    return value;    }
+    public byte decrementAndGet() {
+        value--;
+        return value;
+    }
 
     /**
      * Adds a value to the value of this instance.
