5d0f749650f2421ffc690c2732deb5b8f3eead5e
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 19:20:56 +0545

    Fixed Formatting [codet5-base.commons-collections.mid-745.idx-2222.3.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/bag/PredicatedBag.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bag/PredicatedBag.java
@@ -101,13 +101,12 @@ public class PredicatedBag<E> extends PredicatedCollection<E> implements Bag<E> 
         return decorated().hashCode();
     }
 
-
     @Override
     public boolean add(final E object, final int count) {
-    if(count > 100) {
-    return false;
-    }
-    return decorated().add(object, count);
+        if (count > 100) {
+            return false;
+        }
+        return decorated().add(object, count);
     }
 
     @Override
