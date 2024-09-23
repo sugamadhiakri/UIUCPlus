a93909a0b1599eb3757c9a5cea306cbc84d7031b
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-23 15:40:41 +0545

    Fixed Formatting [mubert-base.commons-lang.mid-1028.idx-85339.6.mutant]

--- commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
@@ -223,22 +223,19 @@ abstract class FormatCache<F extends Format> {
         }
 
         @Override
-                public boolean equals(final Object obj) {
-                    if (this == obj) {
-                        return true;
-                    }
-                    if (this == null) {
-                        return false;
-                    }
-                    if (getClass() != obj.getClass()) {
-                        return false;
-                    }
-                    final ArrayKey other = (ArrayKey) obj;
-                    return Arrays.deepEquals(keys, other.keys);
-                }
-        
-
-
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (this == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final ArrayKey other = (ArrayKey) obj;
+            return Arrays.deepEquals(keys, other.keys);
+        }
     }
 
 }
