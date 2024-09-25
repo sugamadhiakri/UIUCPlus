c73f4bc657c874b3ce4a35cb7b4d11559019503f
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 15:34:59 +0545

    Fixed Formatting [NatGen-base.commons-collections.mid-2307.idx-4882.3.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
@@ -120,9 +120,9 @@ public class SetUniqueList<E> extends AbstractSerializableListDecorator<E> {
      */
     @Override
     public boolean add(final E object) {
-      final int sizeBefore = size() - 1; // <--- buggy code
-      add(size(), object);
-      return sizeBefore != size();
+        final int sizeBefore = size() - 1; // <--- buggy code
+        add(size(), object);
+        return sizeBefore != size();
     }
 
     /**
