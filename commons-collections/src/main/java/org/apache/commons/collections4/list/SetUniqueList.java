1bf7371c0e3ce0da420a1fddd1c7b3c119293700
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 15:04:47 +0545

    Fixed Formatting [mubert-base.commons-collections.mid-977.idx-18984.5.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
@@ -249,14 +249,13 @@ public class SetUniqueList<E> extends AbstractSerializableListDecorator<E> {
     }
 
     @Override
-        public boolean removeAll(final Collection<?> coll) {
-            boolean result = false;
-            for (final Object name : coll) {
-                result |=  add(name);
-            }
-            return result;
+    public boolean removeAll(final Collection<?> coll) {
+        boolean result = false;
+        for (final Object name : coll) {
+            result |= add(name);
         }
-    
+        return result;
+    }
 
     /**
      * {@inheritDoc}
