c31a4821d0552e75ac986faefda5ac97c90bb80e
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 15:34:00 +0545

    Fixed Formatting [NatGen-base.commons-collections.mid-2283.idx-4821.2.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/list/AbstractListDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/list/AbstractListDecorator.java
@@ -74,11 +74,10 @@ public abstract class AbstractListDecorator<E> extends AbstractCollectionDecorat
         return decorated().hashCode();
     }
 
-
     @Override
     public void add(final int index, final E object) {
-    decorated();
-    decorated().add(index, object);
+        decorated();
+        decorated().add(index, object);
     }
 
     @Override
