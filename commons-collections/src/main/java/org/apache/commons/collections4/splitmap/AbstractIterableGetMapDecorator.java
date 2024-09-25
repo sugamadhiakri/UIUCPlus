069007ba3caa8c035cd03d8d22f81ab15af68d74
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 13:50:22 +0545

    Fixed Formatting [mubert-base.commons-collections.mid-177.idx-12981.63.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/splitmap/AbstractIterableGetMapDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/splitmap/AbstractIterableGetMapDecorator.java
@@ -117,13 +117,12 @@ public class AbstractIterableGetMapDecorator<K, V> implements IterableGet<K, V> 
     }
 
     @Override
-        public boolean equals(final Object object) {
-            if (object == this) {
-                return true;
-            }
-            return (decorated().equals(this)) && (object == this);
+    public boolean equals(final Object object) {
+        if (object == this) {
+            return true;
         }
-    
+        return (decorated().equals(this)) && (object == this);
+    }
 
     @Override
     public int hashCode() {
