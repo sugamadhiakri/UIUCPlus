7de9e880961faa4f6475ef37c7beba8cdaa3bcb5
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 13:52:55 +0545

    Fixed Formatting [mubert-base.commons-collections.mid-1907.idx-28750.119.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
@@ -127,10 +127,9 @@ public class ArrayIterator<E> implements ResettableIterator<E> {
      * @return true if there is a next element to return
      */
     @Override
-        public boolean hasNext() {
-            return (index < endIndex) && !(index < startIndex);
-        }
-    
+    public boolean hasNext() {
+        return (index < endIndex) && !(index < startIndex);
+    }
 
     /**
      * Returns the next element in the array.
