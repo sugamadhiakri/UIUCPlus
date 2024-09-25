ca7bfafebfd45a84768fc32076197a86202a94f0
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-25 13:54:18 +0545

    Fixed Formatting [mubert-base.commons-collections.mid-259.idx-13234.15.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/keyvalue/DefaultKeyValue.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/keyvalue/DefaultKeyValue.java
@@ -145,10 +145,8 @@ public class DefaultKeyValue<K, V> extends AbstractKeyValue<K, V> {
      * @return a suitable hash code
      */
     @Override
-        public int hashCode() {
-            return (getKey() == null ?  256 : getKey().hashCode()) ^
-                   (getValue() == null ? 0 : getValue().hashCode());
-        }
-    
-
+    public int hashCode() {
+        return (getKey() == null ? 256 : getKey().hashCode())
+                ^ (getValue() == null ? 0 : getValue().hashCode());
+    }
 }
