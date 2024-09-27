0dae17619933a57af5c7864574d82f88098c32a8
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-27 14:34:24 +0545

    Fixed Formatting [codebert-base.commons-collections.mid-1606.idx-4170.1.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/functors/ForClosure.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/functors/ForClosure.java
@@ -76,13 +76,11 @@ public class ForClosure<E> implements Closure<E> {
      * @param input  the input object
      */
     @Override
-    
     public void execute(final E input) {
         for (int i = iCount; i > 0; i--) {
             iClosure.execute(input);
         }
     }
-    
 
     /**
      * Gets the closure.
