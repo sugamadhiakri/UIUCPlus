25e2e75004401060d45d647b7290c3f70c738a0b
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-27 14:28:48 +0545

    Fixed Formatting [codebert-base.commons-collections.mid-1169.idx-3554.1.mutant]

--- commons-collections/src/main/java/org/apache/commons/collections4/bloomfilter/SparseBloomFilter.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bloomfilter/SparseBloomFilter.java
@@ -80,21 +80,23 @@ public final class SparseBloomFilter implements BloomFilter {
 
     @Override
     public boolean merge(final IndexProducer indexProducer) {
-    if (indexProducer == null) {
-    throw new NullPointerException("indexProducer is null");
-    }
-    indexProducer.forEachIndex(this::add);
-    if (!this.indices.isEmpty()) {
-    if (this.indices.last() >= shape.getNumberOfBits()) {
-    throw new IllegalArgumentException(String.format("Value in list %s is greater than maximum value (%s)",
-    this.indices.last(), shape.getNumberOfBits() - 1));
-    }
-    if (this.indices.first() < 0) {
-    throw new IllegalArgumentException(
-    String.format("Value in list %s is less than 0", this.indices.first()));
-    }
-    }
-    return true;
+        if (indexProducer == null) {
+            throw new NullPointerException("indexProducer is null");
+        }
+        indexProducer.forEachIndex(this::add);
+        if (!this.indices.isEmpty()) {
+            if (this.indices.last() >= shape.getNumberOfBits()) {
+                throw new IllegalArgumentException(
+                        String.format(
+                                "Value in list %s is greater than maximum value (%s)",
+                                this.indices.last(), shape.getNumberOfBits() - 1));
+            }
+            if (this.indices.first() < 0) {
+                throw new IllegalArgumentException(
+                        String.format("Value in list %s is less than 0", this.indices.first()));
+            }
+        }
+        return true;
     }
 
     @Override
