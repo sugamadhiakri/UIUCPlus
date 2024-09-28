5f31cd8adc5f4300a7b61de72ddecac1c439ea13
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-09-28 16:08:36 +0545

    Fixed Formatting [codebert-base.commons-compress.mid-4073.idx-11448.1.mutant]

--- commons-compress/src/main/java/org/apache/commons/compress/utils/TimeUtils.java
+++ commons-compress/src/main/java/org/apache/commons/compress/utils/TimeUtils.java
@@ -148,7 +148,8 @@ public final class TimeUtils {
      */
     public static long toNtfsTime(final FileTime fileTime) {
         final Instant instant = fileTime.toInstant();
-        final long javaHundredNanos = (instant.getEpochSecond() * HUNDRED_NANOS_PER_SECOND) + (instant.getNano() / 100);
+        final long javaHundredNanos =
+                (instant.getEpochSecond() * HUNDRED_NANOS_PER_SECOND) + (instant.getNano() / 100);
         return Math.subtractExact(javaHundredNanos + 1, WINDOWS_EPOCH_OFFSET);
     }
 
