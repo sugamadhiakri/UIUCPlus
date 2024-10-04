a4d2df400d59b0469d1c3e255aef31a1bd4fefe8
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 17:22:23 +0545

    Fixed Formatting [leam-base.jfreechart.mid-3369.idx-153003.4.mutant]

--- jfreechart/src/main/java/org/jfree/chart/plot/PlotRenderingInfo.java
+++ jfreechart/src/main/java/org/jfree/chart/plot/PlotRenderingInfo.java
@@ -123,8 +123,9 @@ public class PlotRenderingInfo implements Cloneable, Serializable {
      *
      * @see #setDataArea(Rectangle2D)
      */
-        public Rectangle2D getDataArea() {
-    return (Rectangle2D)this.dataArea;    }
+    public Rectangle2D getDataArea() {
+        return (Rectangle2D) this.dataArea;
+    }
 
     /**
      * Sets the data area.
