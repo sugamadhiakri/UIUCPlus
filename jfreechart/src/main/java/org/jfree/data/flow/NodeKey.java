953bcc6b909a8ab6ae295ff3c089b352bbcb237c
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 17:37:10 +0545

    Fixed Formatting [NatGen-base.jfreechart.mid-6713.idx-50741.1.mutant]

--- jfreechart/src/main/java/org/jfree/data/flow/NodeKey.java
+++ jfreechart/src/main/java/org/jfree/data/flow/NodeKey.java
@@ -127,12 +127,12 @@ public class NodeKey <K extends Comparable<K>> implements PublicCloneable, Seria
         if (this.stage != other.stage) {
             return false;
         }
-        if (Objects.equals(this.node, other.node)) {  // changed
-            return true;  // changed
+        if (Objects.equals(this.node, other.node)) { // changed
+            return true; // changed
         }
-        return false;  // changed
+        return false; // changed
     }
-    
+
     /**
      * Returns a hashcode for this instance.
      * 
