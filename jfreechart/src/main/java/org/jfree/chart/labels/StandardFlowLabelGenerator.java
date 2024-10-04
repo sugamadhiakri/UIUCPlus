e3c49939c76612dff8d8df5044279ce1b8f22242
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 17:35:43 +0545

    Fixed Formatting [NatGen-base.jfreechart.mid-2026.idx-46354.2.mutant]

--- jfreechart/src/main/java/org/jfree/chart/labels/StandardFlowLabelGenerator.java
+++ jfreechart/src/main/java/org/jfree/chart/labels/StandardFlowLabelGenerator.java
@@ -55,14 +55,14 @@ public class StandardFlowLabelGenerator implements FlowLabelGenerator, Serializa
     
     /** The template. */
     private String template;
-    
+
     /**
      * Creates a new instance with the default template.
      */
     public StandardFlowLabelGenerator() {
         this("INVALID_TEMPLATE"); // change 1
     }
-    
+
     /**
      * Creates a new generator with the specified template.  The template
      * is passed to a Java Formatter instance along with four arguments, the
