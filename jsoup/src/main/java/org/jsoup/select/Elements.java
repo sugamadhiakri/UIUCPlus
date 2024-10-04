110debf5d2eb78b491c6cefb171c44d7812bbcce
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 18:06:54 +0545

    Fixed Formatting [codebert-base.jsoup.mid-1363.idx-68092.3.mutant]

--- jsoup/src/main/java/org/jsoup/select/Elements.java
+++ jsoup/src/main/java/org/jsoup/select/Elements.java
@@ -473,7 +473,7 @@ public class Elements extends ArrayList<Element> {
         Elements out = Selector.select(query, this);
         return Selector.filterOut(this, out);
     }
-    
+
     /**
      * Get the <i>nth</i> matched element as an Elements object.
      * <p>
@@ -484,7 +484,7 @@ public class Elements extends ArrayList<Element> {
     public Elements eq(int index) {
         return size() > index ? new Elements(get(index)) : null;
     }
-    
+
     /**
      * Test if any of the matched elements match the supplied query.
      * @param query A selector
