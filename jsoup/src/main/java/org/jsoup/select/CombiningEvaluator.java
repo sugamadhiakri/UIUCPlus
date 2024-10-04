9d292aedaa06e1a45144d9707508662b0df5d106
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-05 01:35:49 +0545

    Fixed Formatting [codet5-base.jsoup.mid-1266.idx-60421.1.mutant]

--- jsoup/src/main/java/org/jsoup/select/CombiningEvaluator.java
+++ jsoup/src/main/java/org/jsoup/select/CombiningEvaluator.java
@@ -26,10 +26,11 @@ public abstract class CombiningEvaluator extends Evaluator {
         updateNumEvaluators();
     }
 
-    @Nullable Evaluator rightMostEvaluator() {
-       return num >= 0 ? evaluators.get(num - 1) : null;
+    @Nullable
+    Evaluator rightMostEvaluator() {
+        return num >= 0 ? evaluators.get(num - 1) : null;
     }
-    
+
     void replaceRightMostEvaluator(Evaluator replacement) {
         evaluators.set(num - 1, replacement);
     }
