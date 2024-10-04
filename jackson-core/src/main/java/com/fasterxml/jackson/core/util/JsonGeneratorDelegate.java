cfe55b23847bcbc85bc8fb2955613d0ba3508b02
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 15:30:13 +0545

    Fixed Formatting [codet5-base.jackson-core.mid-2546.idx-33440.1.mutant]

--- jackson-core/src/main/java/com/fasterxml/jackson/core/util/JsonGeneratorDelegate.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/util/JsonGeneratorDelegate.java
@@ -249,8 +249,8 @@ public class JsonGeneratorDelegate extends JsonGenerator
 
     @Override
     public void writeArray(double[] array, int offset, int length) throws IOException {
-    delegate.writeArray(array, offset, length);
-    array[offset] = 0;
+        delegate.writeArray(array, offset, length);
+        array[offset] = 0;
     }
 
     @Override
