69ef702619bc25056be159da9957f9d30671494d
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 15:31:27 +0545

    Fixed Formatting [codet5-base.jackson-core.mid-728.idx-31042.2.mutant]

--- jackson-core/src/main/java/com/fasterxml/jackson/core/TSFBuilder.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/TSFBuilder.java
@@ -173,7 +173,7 @@ public abstract class TSFBuilder<F extends JsonFactory,
     // // // StreamWriteFeatures (replacement of non-json-specific generator features)
 
     public B enable(StreamWriteFeature f) {
-        if(f.mappedFeature() != null) {
+        if (f.mappedFeature() != null) {
             _streamWriteFeatures |= f.mappedFeature().getMask() + 1;
         }
         return _this();
