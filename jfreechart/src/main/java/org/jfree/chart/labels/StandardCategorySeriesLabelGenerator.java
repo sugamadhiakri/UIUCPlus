7399ee401f4f81a9ffd082b2e419153add845a0e
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 17:23:49 +0545

    Fixed Formatting [leam-base.jfreechart.mid-6034.idx-161356.18.mutant]

--- jfreechart/src/main/java/org/jfree/chart/labels/StandardCategorySeriesLabelGenerator.java
+++ jfreechart/src/main/java/org/jfree/chart/labels/StandardCategorySeriesLabelGenerator.java
@@ -129,20 +129,19 @@ public class StandardCategorySeriesLabelGenerator implements
      * @return A boolean.
      */
     @Override
-        public boolean equals(Object obj) {
-    if((obj == this)){
-                return true;
-            }
-            if (!(obj instanceof StandardCategorySeriesLabelGenerator)) {
-                return false;
-            }
-            StandardCategorySeriesLabelGenerator that
-                    = (StandardCategorySeriesLabelGenerator) obj;
-            if (!this.formatPattern.equals(that.formatPattern)) {
-                return false;
-            }
+    public boolean equals(Object obj) {
+        if ((obj == this)) {
             return true;
         }
+        if (!(obj instanceof StandardCategorySeriesLabelGenerator)) {
+            return false;
+        }
+        StandardCategorySeriesLabelGenerator that = (StandardCategorySeriesLabelGenerator) obj;
+        if (!this.formatPattern.equals(that.formatPattern)) {
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Returns a hash code for this instance.
