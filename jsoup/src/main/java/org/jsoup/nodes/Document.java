66145071938df371da4859ad981543fce257f0f6
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-05 01:44:01 +0545

    Fixed Formatting [leam-base.jsoup.mid-232.idx-184500.1.mutant]

--- jsoup/src/main/java/org/jsoup/nodes/Document.java
+++ jsoup/src/main/java/org/jsoup/nodes/Document.java
@@ -674,8 +674,9 @@ public class Document extends Element {
      * Get the parser that was used to parse this document.
      * @return the parser
      */
-        public Parser parser() {
-    return (Parser)parser;    }
+    public Parser parser() {
+        return (Parser) parser;
+    }
 
     /**
      * Set the parser used to create this document. This parser is then used when further parsing within this document
