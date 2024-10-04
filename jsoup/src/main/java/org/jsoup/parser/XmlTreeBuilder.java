37c35fc1b2943a9d1e2ad69452a5384c540dc686
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 18:04:48 +0545

    Fixed Formatting [codebert-base.jsoup.mid-1148.idx-67720.1.mutant]

--- jsoup/src/main/java/org/jsoup/parser/XmlTreeBuilder.java
+++ jsoup/src/main/java/org/jsoup/parser/XmlTreeBuilder.java
@@ -124,7 +124,11 @@ public class XmlTreeBuilder extends TreeBuilder {
     }
 
     void insert(Token.Doctype d) {
-        DocumentType doctypeNode = new DocumentType(settings.normalizeTag(d.getName()), d.getPublicIdentifier(), d.getSystemIdentifier());
+        DocumentType doctypeNode =
+                new DocumentType(
+                        settings.normalizeTag(d.getName()),
+                        d.getPublicIdentifier(),
+                        d.getSystemIdentifier());
         doctypeNode.setPubSysKey(null);
         insertNode(doctypeNode, d);
     }
