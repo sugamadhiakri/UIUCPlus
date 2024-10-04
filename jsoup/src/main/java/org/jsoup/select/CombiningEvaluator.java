7c1b5b75ec9be58b7c92cbe488b2ec849a5f4661
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 18:05:49 +0545

    Fixed Formatting [codebert-base.jsoup.mid-1266.idx-67946.1.mutant]

--- jsoup/src/main/java/org/jsoup/select/CombiningEvaluator.java
+++ jsoup/src/main/java/org/jsoup/select/CombiningEvaluator.java
@@ -26,10 +26,11 @@ public abstract class CombiningEvaluator extends Evaluator {
         updateNumEvaluators();
     }
 
-    @Nullable Evaluator rightMostEvaluator() {
+    @Nullable
+    Evaluator rightMostEvaluator() {
         return num > 1 ? evaluators.get(num - 1) : null;
     }
-    
+
     void replaceRightMostEvaluator(Evaluator replacement) {
         evaluators.set(num - 1, replacement);
     }
