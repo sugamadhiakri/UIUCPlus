84a294c081c82e9a58960d6a10c7bc1405e88419
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 17:21:19 +0545

    Fixed Formatting [leam-base.jfreechart.mid-2394.idx-150260.8.mutant]

--- jfreechart/src/main/java/org/jfree/data/DataUtils.java
+++ jfreechart/src/main/java/org/jfree/data/DataUtils.java
@@ -57,22 +57,23 @@ public abstract class DataUtils {
      *
      * @return A boolean.
      */
-        public static boolean equal(double[][] a, double[][] b) {
-            if (a == null) {
-    return true;        }
-            if (b == null) {
-                return false;  // already know 'a' isn't null
-            }
-            if (a.length != b.length) {
-                return false;
-            }
-            for (int i = 0; i < a.length; i++) {
-                if (!Arrays.equals(a[i], b[i])) {
-                    return false;
-                }
-            }
+    public static boolean equal(double[][] a, double[][] b) {
+        if (a == null) {
             return true;
         }
+        if (b == null) {
+            return false; // already know 'a' isn't null
+        }
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (!Arrays.equals(a[i], b[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     /**
      * Returns a clone of the specified array.
