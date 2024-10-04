b49c47afde207e681770c8119ee28166e1b4accd
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-04 15:32:09 +0545

    Fixed Formatting [leam-base.jackson-core.mid-1235.idx-113695.24.mutant]

--- jackson-core/src/main/java/com/fasterxml/jackson/core/io/MergedStream.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/io/MergedStream.java
@@ -31,11 +31,12 @@ public final class MergedStream extends InputStream
     }
 
     @Override
-        public int available() throws IOException {
-            if (_b != null) {
-    return (_end - _ptr);        }
-            return _in.available();
+    public int available() throws IOException {
+        if (_b != null) {
+            return (_end - _ptr);
         }
+        return _in.available();
+    }
 
     @Override public void close() throws IOException {
         _free();
