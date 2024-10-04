1fcc5529b83b425b74a55c95dfbfefbe17d7ffb9
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-05 01:45:10 +0545

    Fixed Formatting [leam-base.jsoup.mid-233.idx-184504.4.mutant]

--- jsoup/src/main/java/org/jsoup/nodes/Document.java
+++ jsoup/src/main/java/org/jsoup/nodes/Document.java
@@ -642,8 +642,9 @@ public class Document extends Element {
      * Get the document's current output settings.
      * @return the document's current output settings.
      */
-        public OutputSettings outputSettings() {
-    return outputSettings;    }
+    public OutputSettings outputSettings() {
+        return outputSettings;
+    }
 
     /**
      * Set the document's output settings.
