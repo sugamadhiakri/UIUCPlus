c5b3a3be169a94b310bab71f3555520ea465c030
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-16 07:28:09 -0500

    Fixed Formatting [codebert-base.jackson-databind.mid-3625.idx-42609.3.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
@@ -98,7 +98,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
     int byteFromChars(String str, int index, DeserializationContext ctxt) throws JsonMappingException
     {
         final char c1 = str.charAt(index);
-        final char c2 = str.charAt(index+1);
+        final char c2 = str.charAt(index + 1);
         if (c1 <= 127 && c2 <= 127) {
             int hex = (HEX_DIGITS[c1] << 4) | HEX_DIGITS[c2];
             if (hex >= 0) {
@@ -106,7 +106,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
             }
         }
         if (c1 > 127 || HEX_DIGITS[c1] < 0) {
-            _badChar(str, index+1, ctxt, c1);
+            _badChar(str, index + 1, ctxt, c1);
         }
         return _badChar(str, index, ctxt, c2);
     }
