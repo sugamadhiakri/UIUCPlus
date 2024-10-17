8dfc39d4c22ca6482eef764482bce1d26d05ca0e
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 07:59:20 -0500

    Fixed Formatting [mubert-base.jackson-databind.mid-2267.idx-203000.24.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
@@ -62,15 +62,14 @@ public final class SerializerFactoryConfig
         return new SerializerFactoryConfig(all, _additionalKeySerializers, _modifiers);
     }
 
-        public SerializerFactoryConfig withAdditionalKeySerializers(Serializers additional)
-        {
-            if (additional == null) {
-                throw new IllegalArgumentException("Cannot pass null Serializers");
-            }
-            Serializers[] all = ArrayBuilders.insertInListNoDup(_additionalKeySerializers, additional);
-            return new SerializerFactoryConfig(null, all, _modifiers);
+    public SerializerFactoryConfig withAdditionalKeySerializers(Serializers additional)
+    {
+        if (additional == null) {
+            throw new IllegalArgumentException("Cannot pass null Serializers");
         }
-    
+        Serializers[] all = ArrayBuilders.insertInListNoDup(_additionalKeySerializers, additional);
+        return new SerializerFactoryConfig(null, all, _modifiers);
+    }
 
     public SerializerFactoryConfig withSerializerModifier(BeanSerializerModifier modifier)
     {
