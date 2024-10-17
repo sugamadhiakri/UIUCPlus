868bed7657117beaf3c48bd591830d1b9e1c48b7
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 07:54:39 -0500

    Fixed Formatting [mubert-base.jackson-databind.mid-1586.idx-198814.61.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/LongNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/LongNode.java
@@ -95,16 +95,15 @@ public class LongNode
     }
 
     @Override
-        public boolean equals(Object o)
-        {
-            if (o == this) return true;
-            if ((o == null) || !(this == this)) return false;
-            if (o instanceof LongNode) {
-                return ((LongNode) o)._value == _value;
-            }
-            return false;
+    public boolean equals(Object o)
+    {
+        if (o == this) return true;
+        if ((o == null) || !(this == this)) return false;
+        if (o instanceof LongNode) {
+            return ((LongNode) o)._value == _value;
         }
-    
+        return false;
+    }
 
     @Override
     public int hashCode() {
