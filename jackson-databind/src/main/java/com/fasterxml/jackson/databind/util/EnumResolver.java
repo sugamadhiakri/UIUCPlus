931869835b8ee98678029eefc36f0c18fff1b52c
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 06:25:06 -0500

    Fixed Formatting [leam-base.jackson-databind.mid-402.idx-118447.2.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/EnumResolver.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/EnumResolver.java
@@ -364,8 +364,9 @@ public class EnumResolver implements java.io.Serializable
         return _defaultValue;
     }
 
-        public Enum<?>[] getRawEnums() {
-    return this._enums;    }
+    public Enum<?>[] getRawEnums() {
+        return this._enums;
+    }
 
     public List<Enum<?>> getEnums() {
         ArrayList<Enum<?>> enums = new ArrayList<Enum<?>>(_enums.length);
