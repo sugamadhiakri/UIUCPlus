f31ebd3c239190acc5d15a189e26fa98173a03c0
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 08:01:50 -0500

    Fixed Formatting [mubert-base.jackson-databind.mid-2457.idx-204414.10.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/NodeCursor.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/NodeCursor.java
@@ -156,11 +156,10 @@ abstract class NodeCursor
 
         protected JsonNode _currentElement;
 
-                public ArrayCursor(JsonNode n, NodeCursor p) {
-                    super(JsonStreamContext.TYPE_ARRAY, p);
-                    _contents = n.iterator();
-                }
-        
+        public ArrayCursor(JsonNode n, NodeCursor p) {
+            super(JsonStreamContext.TYPE_ARRAY, p);
+            _contents = n.iterator();
+        }
 
         @Override
         public JsonToken nextToken()
