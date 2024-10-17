9bec8af495cd07331d9e651414119f419317ebd0
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 03:26:06 -0500

    Fixed Formatting [leam-base.jackson-databind.mid-3211.idx-129401.10.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
@@ -229,8 +229,9 @@ public class StdValueInstantiator
     }
 
     @Override
-        public boolean canCreateUsingArrayDelegate() {
-    return (this._arrayDelegateType != null);    }
+    public boolean canCreateUsingArrayDelegate() {
+        return (this._arrayDelegateType != null);
+    }
 
     @Override
     public boolean canCreateFromObjectWith() {
