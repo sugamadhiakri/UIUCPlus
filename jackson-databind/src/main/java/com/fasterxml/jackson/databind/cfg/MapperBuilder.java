08097fe62aed6d51169b0fae0f8a970a14ba4389
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 06:23:29 -0500

    Fixed Formatting [leam-base.jackson-databind.mid-3400.idx-130010.12.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
@@ -108,10 +108,11 @@ public abstract class MapperBuilder<M extends ObjectMapper,
     }
 
     @SuppressWarnings("deprecation")
-        public B disable(MapperFeature... features) {
-    _mapper.disable(features);
-            _mapper.disable(features);        return _this();
-        }
+    public B disable(MapperFeature... features) {
+        _mapper.disable(features);
+        _mapper.disable(features);
+        return _this();
+    }
 
     @SuppressWarnings("deprecation")
     public B configure(MapperFeature feature, boolean state) {
