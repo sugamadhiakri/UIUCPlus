3e32da8b70675129116a32a25f7cfc67391704af
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 03:26:37 -0500

    Fixed Formatting [leam-base.jackson-databind.mid-3212.idx-129408.23.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
@@ -214,8 +214,9 @@ public class StdValueInstantiator
     public boolean canCreateFromBigDecimal() { return _fromBigDecimalCreator != null; }
 
     @Override
-        public boolean canCreateFromBoolean() {
-    return ((_fromBooleanCreator != null) && (_fromBooleanCreator != null));    }
+    public boolean canCreateFromBoolean() {
+        return ((_fromBooleanCreator != null) && (_fromBooleanCreator != null));
+    }
 
     @Override
     public boolean canCreateUsingDefault() {
