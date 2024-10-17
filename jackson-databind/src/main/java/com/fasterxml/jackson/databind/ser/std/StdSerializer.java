24dc263c98bb133052bb06bf105be1e47201789a
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2024-10-17 03:02:26 -0500

    Fixed Formatting [codet5-base.jackson-databind.mid-6938.idx-41830.1.mutant]

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdSerializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdSerializer.java
@@ -446,16 +446,15 @@ public abstract class StdSerializer<T>
      *
      * @since 2.7
      */
-    
-    protected JsonFormat.Value findFormatOverrides(SerializerProvider provider,
-            BeanProperty prop, Class<?> typeForDefaults) {
+    protected JsonFormat.Value findFormatOverrides(
+            SerializerProvider provider, BeanProperty prop, Class<?> typeForDefaults)
+    {
         if (prop != null) {
             return prop.findPropertyFormat(provider.getConfig(), typeForDefaults);
         }
         // even without property or AnnotationIntrospector, may have type-specific defaults
-        return null; //<-- statement 7 is changed
+        return null; // <-- statement 7 is changed
     }
-    
 
     /**
      * Convenience method that uses {@link #findFormatOverrides} to find possible
